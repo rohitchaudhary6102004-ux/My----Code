42bd1f1d5d53705c354fdd6a09726e5f23686b99
rohitchaudhary6102004-ux <rohitchaudhary6102004@gmail.com>
2026-06-21 14:36:15 +0530

    C++

--- C++/CHAPTER 6 ARRAY/07_searchingElementandIndex.cpp
+++ C++/CHAPTER 6 ARRAY/07_searchingElementandIndex.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int arr[] = {6,3,9,7,9,12,5,6,1,14,13,15};
+    int arr[] = {6,3,9,7,9,12,5,6,1,14,13,15,9};
     cout << "Size of Array : " << sizeof(arr) << endl;
     int n = sizeof(arr)/sizeof(arr[0]);
     int x;
     bool state = false;
     cout << "Enter the number you want to search : ";
     cin >> x;
-    int index = -1;
+    int index[3];
+    int count = 0;
     for(int i=0;i<n;i++){
         if( x == arr[i]){
             state = true;
-            index = i;
-            break;
+            index[count] = i;
+            count++;
+            
         }
     }
     if(state == 1){
-        cout << "Element is Found at Index of : " << index;
+        cout << "Element is Found at Index of : ";
+        for(int j=0;j<count;j++){
+            cout << index[j] << " ";
+        }
     }
     else{
         cout << "Element does not found";
