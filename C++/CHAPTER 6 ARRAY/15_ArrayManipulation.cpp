ce35c850313b2c9df2aa495b0a68c608b4be4e39
rohitchaudhary6102004-ux <rohitchaudhary6102004@gmail.com>
2026-06-24 21:24:46 +0530

    C++

---
+++ C++/CHAPTER 6 ARRAY/15_ArrayManipulation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[] = {2,3,1,3,2,4,4,5,1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i] == arr[j]){
+                arr[i] = -1;
+                arr[j] = -1;
+            }
+        }
+    }
+    cout << endl;
+    for(int i=0;i<n;i++){
+        if(arr[i] != -1){
+            cout << "The unique Number is : " << arr[i];
+        }
+    }
+}
