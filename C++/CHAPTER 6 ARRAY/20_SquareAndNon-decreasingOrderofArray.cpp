2b69ea2bdd425b128ca5ea371a0597eef82e4da6
rohitchaudhary6102004-ux <rohitchaudhary6102004@gmail.com>
2026-07-02 00:15:50 +0530

    C++

--- C++/CHAPTER 6 ARRAY/20_SquareAndNon-decreasingOrderofArray.cpp
+++ C++/CHAPTER 6 ARRAY/20_SquareAndNon-decreasingOrderofArray.cpp
@@ -5,17 +5,22 @@ int main(){
     int n = sizeof(arr)/sizeof(arr[0]);
     int left_ptr = 0;
     int right_ptr = n-1;
+    int ans[n];
+    int k = n-1;
     while(left_ptr <= right_ptr){
         if(abs(arr[left_ptr]) > abs(arr[right_ptr])){
-            arr[left_ptr] = arr[left_ptr]*arr[left_ptr];
+            ans[k] = arr[left_ptr]*arr[left_ptr];
+            left_ptr++;
+            k--;
         }
         else{
-            arr[right_ptr] = arr[right_ptr]*arr[right_ptr];
+            ans[k] = arr[right_ptr]*arr[right_ptr];
+            right_ptr--;
+            k--;
         }
     }
     for(int i=0;i<n;i++){
-        cout << " Hello" << endl;
-        cout << arr[i] << " ";
+        cout << ans[i] << " ";
     }
     return 0;
 }
