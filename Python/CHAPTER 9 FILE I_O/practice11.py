c5b564020b015ce30ab9a3d52472344a2fa5b208
rohitchaudhary6102004-ux <rohitchaudhary6102004@gmail.com>
2026-07-16 22:15:54 +0530

    py

--- Python/CHAPTER 9 FILE I_O/practice11.py
+++ Python/CHAPTER 9 FILE I_O/practice11.py
@@ -1,1 +1,7 @@
 # Write a python program to rename a file to "renamed_by_python.txt".
+
+with open("file.txt") as f:
+    content = f.read()
+
+with open("file_renamed_by_python", "w") as f:
+    f.write(content)    
--- Python/CHAPTER 9 FILE I_O/practice9.py
+++ Python/CHAPTER 9 FILE I_O/practice9.py
@@ -1,1 +1,11 @@
 # Write a program to find out whether a file is identical & matches the content of another file.
+
+with open("this.txt") as f :
+    content1 = f.read()
+    
+with open("this_copy.txt") as f:
+    content2 = f.read()    
+if ( content1 == content2):
+    print("Content Matched")
+else:
+    print("Content does not Matched")        
