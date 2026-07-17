a52cd2b0f7242ead5a50f31513447d9a05fa3bc7
rohitchaudhary6102004-ux <rohitchaudhary6102004@gmail.com>
2026-07-18 01:00:14 +0530

    pt

--- Python/CHAPTER 11 INHERITANCE/practice5.py
+++ Python/CHAPTER 11 INHERITANCE/practice5.py
@@ -1,2 +1,32 @@
 # Write a class vector representing a vector of n dimensions. Override the + and * operator which calculates the sum and the dot(.) product of them.
 
+class Vector:
+
+    def __init__(self, values):
+        self.values = values
+
+    def __add__(self, other):
+        result = []
+
+        for i in range(len(self.values)):
+            result.append(self.values[i] + other.values[i])
+
+        return Vector(result)
+
+    def __mul__(self, other):
+        dot = 0
+
+        for i in range(len(self.values)):
+            dot += self.values[i] * other.values[i]
+
+        return dot
+
+    def __str__(self):
+        return str(self.values)
+
+
+v1 = Vector([1, 2, 3])
+v2 = Vector([4, 5, 6])
+
+print("Addition :", v1 + v2)
+print("Dot Product :", v1 * v2)
--- Python/CHAPTER 11 INHERITANCE/practice6.py
+++ Python/CHAPTER 11 INHERITANCE/practice6.py
@@ -1,4 +1,18 @@
 # Write a __str__() method to print the vector as follows:
             #  7i + 8j + 10k
     # Assume a vector of dimension 3 for this problem
-            
+
+class Vector:
+
+    def __init__(self, i, j, k):
+        self.i = i
+        self.j = j
+        self.k = k
+
+    def __str__(self):
+        return f"{self.i}i + {self.j}j + {self.k}k"
+
+
+v = Vector(7, 8, 10)
+
+print(v)            
